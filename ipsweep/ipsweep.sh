d259ad3998b4fdf261a4e8b4a1c64f38b889ead1
HackHikeHang <eproctor1978@gmail.com>
2020-04-12 08:30:15 -0400

    fixed ipsweep.sh to use non-deprecated ip instead of ifconfig and script is functional.

--- ipsweep/ipsweep.sh
+++ ipsweep/ipsweep.sh
@@ -2,15 +2,14 @@
 
 if [ "$1" == "" ]
 then
-$network = [ `ifconfig | grep "inet " -m 1 | cut -d " " -f 10 | cut -d . -f 1-3` ] ;
-#echo "You forgot an IP address!"
-#echo ""
-#echo "Using : $network"
-#else
-#$network = $1
+  ip_network="$(ip route | grep "default" | cut -d " " -f 3 | cut -d . -f 1-3)"
+  echo "You forgot an IP address!  Using your default route of $ip_network."
+else
+  ip_network="$1"
 fi
-#for ip in `seq 1 254`; do
-#ping -c 1 $network.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
-#done
 
-echo $network
+for ip in `seq 1 254`; do
+  ping -c 1 $ip_network.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+done
+
+
