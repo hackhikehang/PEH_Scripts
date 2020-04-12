07540a736532ec193dd854b6b1d03fddbdd71f1a
HackHikeHang <eproctor1978@gmail.com>
2020-04-12 15:06:03 -0400

    first python script added

---
+++ python/first.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+#print string
+print("Hello world!")
+print('Yo, world!')
+print()
+
+# triple quotes give multi-line strings
+print("""This string runs
+over multiple lines""")
+print()
+print("This string is " + "awesome!")
