145df16fff3ba0d1059c1812c676352dc40cf6f4
HackHikeHang <eproctor1978@gmail.com>
2020-04-12 14:52:16 -0400

    finished product for now.  Uses the system's default route to compute the /24 network unless you choose to specify a network.

--- ipsweep/ipsweep.sh
+++ ipsweep/ipsweep.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+# ipsweep.sh
+# Uses default route if no network is submitted.
+# format expected:  ./ipsweep.sh 192.168.1
+
 
 if [ "$1" == "" ]
 then
   ip_network="$(ip route | grep "default" | cut -d " " -f 3 | cut -d . -f 1-3)"
-  echo "You forgot an IP address!  Using your default route of $ip_network."
 else
   ip_network="$1"
 fi
@@ -11,5 +14,3 @@ fi
 for ip in `seq 1 254`; do
   ping -c 1 $ip_network.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
 done
-
-
