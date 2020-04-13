f3d087380c88c986029b12d2b485c61a84a2f50a
HackHikeHang <eproctor1978@gmail.com>
2020-04-13 08:12:34 -0400

    math.py added

---
+++ python/math.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+# Math
+
+print (50+50) # addition
+print (50*50) # multiplication
+print (50/50) # division
+print (50**2) # exponents
+print (50-50) # subtraction
+print (50%6) # modulo
+print (50/6) #division
+print (50//6)  #no leftovers
+
+
+
+
+
+
